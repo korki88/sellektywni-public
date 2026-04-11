e86dc2c0d4d7a35242586c0c425d6371afabe673
korki88 <korki88@users.noreply.github.com>
2026-04-11 14:01:11 +0200

    fix: rejestracja middleware dla staff products/customers

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,9 @@ import { OrderModule } from './order/order.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { StaffController } from './staff/staff.controller';
+import { StaffCustomersController } from './staff/staff-customers.controller';
 import { StaffModule } from './staff/staff.module';
+import { StaffProductsController } from './staff/staff-products.controller';
 
 @Module({
   imports: [
@@ -39,7 +41,7 @@ export class AppModule implements NestModule {
 
     consumer
       .apply(SupabaseJwtMiddleware, LoadProfileMiddleware, StaffRoleMiddleware)
-      .forRoutes(StaffController);
+      .forRoutes(StaffController, StaffProductsController, StaffCustomersController);
 
     consumer.apply(SupabaseJwtMiddleware).forRoutes(AuthController);
   }
